ff1b73de6a031c3c964bee438eb8af4528b27293
eagyemang21 <elijahagyemang9@gmail.com>
2022-05-12 12:38:33 -0400

    routes added

--- Routers/routers.js
+++ Routers/routers.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router()
 
+
+router.get('/', baseline)
+
 //user accounts
-router.get()
+router.post('/login', login)
+router.post('/signup', signUp)
+//user 
+router.get('/user/:id', displayUserInfo)
+router.put('/user/:id/update', updateInfo)
+//discussion board
+router.get('/board', displayBoard)
+router.post('/board/post', addPost)
+router.put('/board/post/:id', updatePost)
+router.delete('/board/post/:id', deletePost)
+//programs
+router.post('/program', showProgramInfo)
+router.put('/program/:id', updateProgramInfo)
+router.get('/program', displayProgramInfo)
