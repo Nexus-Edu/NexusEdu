0e04dc63717cf14689171d7309e0c69aa888fd50
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-05-12 11:14:01 -0400

    Migration set up complete

---
+++ migrations/20220512040224_migrations.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+
+  })
+  .createTable('posts', table => {
+
+  })
+  .createTable('comments', table => {
+
+})
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
