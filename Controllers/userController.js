43f276c68dd21ef3c3cb6939161ecb12bc29eaa6
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-05-15 07:11:30 -0400

    User Router Completed

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,16 +1,42 @@
+const pool = require('../Database-Config/db');
+const User = require("../Models/userModel")
+
+async function getAllUserAccounts(req, res) {
+    const result = await User.getAllUserAccounts(); 
+    res.status(200).json(result)
+}
+
 // Gets all user account information
 // GET request
 async function displayUserInfo(req, res) {
+    const id = req.params.id;
+    console.log(id)
 
+    try {
+        const displayUser = await User.displayUserInfo(id);
+        res.status(200).json(displayUser);
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
 }
 
 // Edit updates all or specific parts of a user's account
 // PUT request
 async function updateInfo(req, res) {
+const id = req.params.id;
+const { email, first_name, last_name, age, birthday, image, bio } = req.body;
 
+try {
+    const updateUserInfo = await User.updateInfo(id, email, first_name, last_name, age, birthday, image, bio);
+
+    res.status(200).json(updateUserInfo)
+} catch (err) {
+    res.status(500).send({ message: err.message })
+}
 }
 
 module.exports = {
     displayUserInfo,
-    updateInfo
+    updateInfo,
+    getAllUserAccounts
 }
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // Controller Imports
-const { displayUserInfo, updateInfo } = require('../Controllers/userController')
+const { displayUserInfo, updateInfo, getAllUserAccounts } = require('../Controllers/userController')
 
+// user
+// A Route used to get all user account information
+// Delete route when project is done
+router.get('/', getAllUserAccounts)
 // user/:id
 router.get('/:id', displayUserInfo)
 // user/:id/update
