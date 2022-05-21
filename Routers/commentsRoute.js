b2b4bd1bb24141953a4c9d3ebf5762d4bf967093
leo <cedillo47@gmail.com>
2022-05-21 02:57:26 -0400

    fixed comments bug

--- Routers/commentsRoute.js
+++ Routers/commentsRoute.js
@@ -10,7 +10,8 @@ router.get('/:id', async (req, res) => {
         from comments 
         join users 
         on comments.user_id = users.id
-        where comments.post_id = $1`,[id]);
+        where comments.post_id = $1
+        ORDER BY posts.id DESC`,[id]);
         return res.status(200).json({ data: databaseResult.rows })
     }
     catch {
@@ -21,14 +22,14 @@ router.get('/:id', async (req, res) => {
 router.post('/makeComment', async (req, res) => {
     const data = req.body;
     try {
-        const response = await pool.query(`INSERT INTO comments (post_id, user_id, comment) values ($1, $2, $3) returning *;`, [data.user_id, data.post_id, data.comment]) 
+        const response = await pool.query(`INSERT INTO comments (post_id, user_id, comment) values ($1, $2, $3) returning *;`, [data.post_id, data.user_id, data.comment]) 
 
         res.status(201).json({ data: response.rows });
     } catch (err) {
         res.status(500).json({ message: `${err.message}` });
     }
 }) 
-
+// -> there was a change here need to save and then update the delete 
 
 router.delete('/delete/:id', async (req, res)=>{
     const id = req.params.id
